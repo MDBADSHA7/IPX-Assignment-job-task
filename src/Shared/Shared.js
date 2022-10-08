72424690b1333d45edd08dd524a8678693bf385b
MDBADSHA7 <mahabubhossainfozlu@gmail.com>
2022-10-08 15:52:31 +0600

    first commit

---
+++ src/Shared/Shared.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Navbar from '../Navbar/Navbar';
+
+const Shared = () => {
+    return (
+        <div><Navbar></Navbar></div>
+    );
+};
+
+export default Shared;
